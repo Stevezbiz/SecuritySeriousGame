11f115e1077ba0f41b4025389613fde919027398
Stevezbiz <s270227@studenti.polito.it>
2021-09-14 16:57:25 +0200

    code refactor
    simplified list visual management

--- SeriousGame/Assets/Scripts/Shop.cs
+++ SeriousGame/Assets/Scripts/Shop.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour {
     [SerializeField] GameObject shopItem;
-    [SerializeField] Transform spawnPoint;
     [SerializeField] RectTransform content;
     [SerializeField] TextAsset shopFileJSON;
     [SerializeField] GameObject details;
     [SerializeField] ShopGUI shop;
-
-    int height = 100;
-    int count = 0;
 
     // Start is called before the first frame update
     void Start() {
@@ -29,16 +25,13 @@ public class Shop : MonoBehaviour {
 
     void AddShopRecord(ShopItemInfo shopItemInfo) {
         // create the new item
-        count++;
-        Vector3 newPos = new Vector3(0, -content.sizeDelta.y, 0);
-        GameObject newRecord = Instantiate(shopItem, newPos, Quaternion.identity);
-        newRecord.transform.SetParent(spawnPoint, false);
+        GameObject newRecord = Instantiate(shopItem);
+        newRecord.transform.SetParent(content, false);
         ShopItem newShopItem = newRecord.GetComponent<ShopItem>();
         newShopItem.Id = shop.AddItem(shopItemInfo);
         newShopItem.SpawnPoint = details.GetComponent<RectTransform>();
         newShopItem.Shop = shop;
         newRecord.name = "ShopItem" + newShopItem.Id.ToString();
-        content.sizeDelta = new Vector2(0, count * height);
     }
 
     public void OpenList() {
