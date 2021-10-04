088f2d67a695aa251c3b292ade08cd297dec7153
Stevezbiz <s270227@studenti.polito.it>
2021-10-04 10:30:06 +0200

    new feature
    added explanation of values in the money view

--- SeriousGame/Assets/Scripts/MoneyView.cs
+++ SeriousGame/Assets/Scripts/MoneyView.cs
@@ -13,6 +13,7 @@ public class MoneyView : MonoBehaviour {
     [SerializeField] TextMeshProUGUI usersModText;
     [SerializeField] TextMeshProUGUI attackUsersMalusText;
     [SerializeField] TextMeshProUGUI actualUsersGainText;
+    [SerializeField] GameObject windowPopUp;
 
     float oldTimeScale = 1f;
 
@@ -27,16 +28,16 @@ public class MoneyView : MonoBehaviour {
         attackMoneyMalusText.SetText("- " + val.ToString("0.##"));
         moneyMalusText.SetText("- " + gameManager.GetMoneyMalus().ToString());
         val = gameManager.GetActualMoneyGain();
-        if (val > 0) actualMoneyGainText.SetText(val.ToString("0.##"));
+        if (val >= 0) actualMoneyGainText.SetText(val.ToString("0.##"));
         else actualMoneyGainText.SetText("- " + (-val).ToString("0.##"));
         // set the users part
         usersGainText.SetText(gameManager.GetUsersGain().ToString());
         val = (gameManager.GetUsersMod() - 1) * 100;
-        if (val > 0) usersModText.SetText(val.ToString("0.##"));
+        if (val >= 0) usersModText.SetText(val.ToString("0.##"));
         else usersModText.SetText("- " + (-val).ToString("0.##"));
         attackUsersMalusText.SetText("- " + gameManager.GetAttackUsersMalus().ToString());
         val = gameManager.GetActualUsersGain();
-        if (val > 0) actualUsersGainText.SetText(val.ToString());
+        if (val >= 0) actualUsersGainText.SetText(val.ToString());
         else actualUsersGainText.SetText("- " + (-val).ToString());
     }
 
@@ -57,4 +58,60 @@ public class MoneyView : MonoBehaviour {
         Time.timeScale = oldTimeScale;
         gameObject.SetActive(false);
     }
+
+    /**
+     * <summary>Open a window with a brief explanation of the value</summary>
+     */
+    public void MoneyGainButton() {
+        GameObject newWindow = Instantiate(windowPopUp, new Vector3(0, 0, 0), Quaternion.identity);
+        newWindow.transform.SetParent(gameObject.transform, false);
+        newWindow.GetComponent<WindowPopUp>().Load("Guadagno utenti: numero di fondi guadagnati in base al numero di utenti che utilizzano il servizio");
+    }
+
+    /**
+     * <summary>Open a window with a brief explanation of the value</summary>
+     */
+    public void MoneyMalusButton() {
+        GameObject newWindow = Instantiate(windowPopUp, new Vector3(0, 0, 0), Quaternion.identity);
+        newWindow.transform.SetParent(gameObject.transform, false);
+        newWindow.GetComponent<WindowPopUp>().Load("Costi: fondi spesi per mantenere attivi servizi e difese");
+    }
+
+    /**
+     * <summary>Open a window with a brief explanation of the value</summary>
+     */
+    public void AttackMoneyMalusButton() {
+        GameObject newWindow = Instantiate(windowPopUp, new Vector3(0, 0, 0), Quaternion.identity);
+        newWindow.transform.SetParent(gameObject.transform, false);
+        newWindow.GetComponent<WindowPopUp>().Load("Malus attacchi: riduzione applicata al guadagno utenti, dovuta agli attacchi in corso");
+    }
+
+    /**
+         * <summary>Open a window with a brief explanation of the value</summary>
+         */
+    public void UsersGainButton() {
+        GameObject newWindow = Instantiate(windowPopUp, new Vector3(0, 0, 0), Quaternion.identity);
+        newWindow.transform.SetParent(gameObject.transform, false);
+        newWindow.GetComponent<WindowPopUp>().Load("Nuovi utenti: numero di nuovi utenti che si iscrivono al servizio");
+    }
+
+    /**
+         * <summary>Open a window with a brief explanation of the value</summary>
+         */
+    public void UsersModButton() {
+        GameObject newWindow = Instantiate(windowPopUp, new Vector3(0, 0, 0), Quaternion.identity);
+        newWindow.transform.SetParent(gameObject.transform, false);
+        newWindow.GetComponent<WindowPopUp>().Load("Modificatore usabilità: modificatore applicato al numero di nuovi utenti. Indica il grado di semplicità/difficoltà che gli utenti incontrano nell'utilizzare il servizio");
+    }
+
+    /**
+         * <summary>Open a window with a brief explanation of the value</summary>
+         */
+    public void AttackUsersMalusButton() {
+        GameObject newWindow = Instantiate(windowPopUp, new Vector3(0, 0, 0), Quaternion.identity);
+        newWindow.transform.SetParent(gameObject.transform, false);
+        newWindow.GetComponent<WindowPopUp>().Load("Malus attacchi: riduzione applicata al numero di nuovi utenti, dovuta agli attacchi in corso");
+    }
+
+
 }
