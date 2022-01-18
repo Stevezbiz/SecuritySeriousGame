5ef458b1a5bb1d8dfa4b11f794db169e1bab4332
Stevezbiz <s270227@studenti.polito.it>
2022-01-18 16:53:40 +0100

    bugfix
    fixed visualization bugs in employee view

--- SeriousGame/Assets/Scripts/RepairView.cs
+++ SeriousGame/Assets/Scripts/RepairView.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class RepairView : MonoBehaviour {
     [SerializeField] GameManager gameManager;
@@ -12,6 +13,11 @@ public class RepairView : MonoBehaviour {
     [SerializeField] TextMeshProUGUI nameText;
     [SerializeField] TextMeshProUGUI descriptionText;
     [SerializeField] TextMeshProUGUI moneyGainText;
+    [SerializeField] Image networkBar;
+    [SerializeField] Image accessBar;
+    [SerializeField] Image softwareBar;
+    [SerializeField] Image assetBar;
+    [SerializeField] Image servicesBar;
     [SerializeField] GameObject networkOutline;
     [SerializeField] GameObject accessOutline;
     [SerializeField] GameObject softwareOutline;
@@ -23,9 +29,15 @@ public class RepairView : MonoBehaviour {
 
     public void Load(EmployeeCode id) {
         EmployeeInfo e = gameManager.GetEmployee(id);
-        nameText.SetText(e.name);
+        nameText.SetText(e.name.ToLower());
         descriptionText.SetText(e.description);
         moneyGainText.SetText(e.moneyGain.ToString());
+        Dictionary<Category, float> abilities = EmployeeUtils.GetAbilities(e.abilities);
+        networkBar.fillAmount = abilities[Category.NETWORK] / 10;
+        accessBar.fillAmount = abilities[Category.ACCESS] / 10;
+        softwareBar.fillAmount = abilities[Category.SOFTWARE] / 10;
+        assetBar.fillAmount = abilities[Category.ASSET] / 10;
+        servicesBar.fillAmount = abilities[Category.SERVICES] / 10;
         tasks.Clear();
         tasks = gameManager.GetAvailableTasksByType(TaskType.REPAIR);
         if (tasks.Count == 0) {
